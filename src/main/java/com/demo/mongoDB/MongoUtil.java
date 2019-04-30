91ce24d99dad1f1b539d0fef37332fa2d86d541c
zhouj <1197186211@qq.com>
2019-04-30 11:43:23 +0800

    修改MongoDB操作

--- src/main/java/com/demo/mongoDB/MongoUtil.java
+++ src/main/java/com/demo/mongoDB/MongoUtil.java
@@ -8,6 +8,7 @@ import com.mongodb.client.MongoDatabase;
 //mongodb 连接数据库工具类
 public class MongoUtil {
     public static MongoDatabase getConnect(){
+        //root:root 账号：密码
         MongoClientURI uri = new MongoClientURI("mongodb://root:root@192.169.7.20:27017/gm");
         MongoClient client = new MongoClient(uri);
         MongoDatabase db = client.getDatabase(uri.getDatabase());
