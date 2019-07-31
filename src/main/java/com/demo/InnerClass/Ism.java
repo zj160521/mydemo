df59561ed24b8435d12ae1135c4621bde37c1766
zhouj <1197186211@qq.com>
2019-07-31 11:30:03 +0800

    内部类

---
+++ src/main/java/com/demo/InnerClass/Ism.java
@@ -0,0 +1,11 @@
+package com.demo.InnerClass;
+
+/**
+ * @Description:
+ * @Author: zhouj
+ * @Date: 2019/7/31 11:27
+ */
+public interface Ism {
+    public void send();
+    public void print();
+}
