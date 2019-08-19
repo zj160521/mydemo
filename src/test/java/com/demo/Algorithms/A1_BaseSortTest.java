3cd73ada29d94aefcedbae00112446b223772e46
zhouj <1197186211@qq.com>
2019-08-19 15:23:19 +0800

    添加测试

---
+++ src/test/java/com/demo/Algorithms/A1_BaseSortTest.java
@@ -0,0 +1,18 @@
+package com.demo.Algorithms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * @Description:
+ * @Author: zhouj
+ * @Date: 2019/7/31 16:31
+ */
+public class A1_BaseSortTest {
+
+    @Test
+    public void bubbleSort() {
+        Assert.assertNotNull("sadf");
+    }
+}
