2b91cb4fff867e4bede9c36725494567032a4f00
zhouj <admin@example.com>
2020-07-22 12:09:12 +0800

    tag test3

--- src/main/java/com/demo/date/test.java
+++ src/main/java/com/demo/date/test.java
@@ -6,7 +6,7 @@ public class test {
 
 	public static void main(String[] args) throws ParseException {
 		String s = "2018-05-17 00:01:01";
-		String s2 = "2018-05-18 00:00:01";
+		String s2 = "2018-05-18 00:00:05";
 		
 		LongStringVo sec = StaticUtilMethod.longToTimeFormat(s, s2);
 		System.out.println(sec.getTimCast());
