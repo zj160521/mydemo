0a93baaf9c0edb0672857f09e906d46c81073928
zhouj <zhouj@singcheer.com>
2022-12-01 09:15:23 +0800

    style(clone): socket注释添加

--- src/main/java/com/demo/io_Serializable/socket/TestSocketServer.java
+++ src/main/java/com/demo/io_Serializable/socket/TestSocketServer.java
@@ -9,21 +9,30 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * @author zhoujie
+ */
 public class TestSocketServer {
 	ServerSocket serverSocket;
 	ExecutorService executor;
 
+	/**
+	 * 初始化线程池和serverSocket
+	 * @throws IOException
+	 */
 	public TestSocketServer() throws IOException {
 		executor = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
 				60L, TimeUnit.SECONDS, new SynchronousQueue<>());
 		serverSocket = new ServerSocket();
-		serverSocket.bind(new InetSocketAddress("localhost", 998));
+		serverSocket.bind(new InetSocketAddress(998));
 	}
 
 	public static void main(String[] args) throws IOException {
 		TestSocketServer testSocket = new TestSocketServer();
 		while (true) {
-			Socket sc = testSocket.serverSocket.accept(); // accept()是阻塞方法，有调用才会执行，否则一直阻塞在这里
+			// accept()是阻塞方法，有调用才会执行，否则一直阻塞在这里
+			Socket sc = testSocket.serverSocket.accept();
+			// 每个socket客户端需要一个线程取处理连接，否则会阻塞
 			testSocket.executor.submit(new TestServerThread(sc));
 		}
 	}
